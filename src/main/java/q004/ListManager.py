bc92b30f1e15a946b32eb8d57b10964c347f2470
bg823 <nagaoka0132@gmail.com>
2020-05-01 17:41:41 +0900

    WIP : sort algorithm made by me.

---
+++ src/main/java/q004/ListManager.py
@@ -0,0 +1,44 @@
+
+import random
+import numpy as np
+
+
+# ListManager class
+class ListManager:
+    dataList = []
+    compareCount = 0
+    exchangeCount = 0
+
+    def __init__(self):
+        # 100のデータ要素が入るlist
+        self.dataList = [0] * 100
+        # 配列の長さ分0-10000までのランダムな数字を追加していく
+        for i in range(len(self.dataList)):
+            self.dataList[i] = random.randint(0, 10000)
+
+    # 2つのデータを比較する
+    # np.signでjavaのsignumと同じことができる
+    def compare(self, index1: int, index2: int) -> int:
+        self.compareCount += 1
+        return int(np.sign(self.dataList[index1] - self.dataList[index2]))
+
+    # 2つのデータを入れ替える
+    def exchange(self, index1: int, index2: int):
+        self.exchangeCount += 1
+        tmp = self.dataList[index1]
+        self.dataList[index1] = self.dataList[index2]
+        self.dataList[index2] = tmp
+
+    # ソートが正しく行われたかをチェックする
+    def checkResult(self):
+        data = self.dataList[0]
+        for i in range(1, len(self.dataList)):
+            if data > self.dataList[i]:
+                raise Exception(
+                    f"ソートされていない: [{(i - 1)}]={data}[{i}]={self.dataList[i]}")
+            data = self.dataList[i]
+        print(f"ソートOK: 比較={self.compareCount}入れ替え={self.exchangeCount}")
+
+    # データのサイズを取得する
+    def size(self) -> int:
+        return len(self.dataList)
---
+++ src/main/java/q004/Q004.py
@@ -0,0 +1,37 @@
+"""
+Q004 ソートアルゴリズム
+
+ListManagerクラスをnewして、小さい順に並び変えた上でcheckResult()を呼び出してください。
+
+実装イメージ:
+ListManager data = new ListManager();
+# TODO 並び換え
+data.checkResult();
+
+- ListManagerクラスを修正してはいけません
+- ListManagerクラスの dataList を直接変更してはいけません
+- ListManagerクラスの比較 compare と入れ替え exchange を使って実現してください
+"""
+
+
+from ListManager import ListManager
+
+
+#
+def sort(dataList: list):
+    for num1 in range(len(dataList)):
+        for num2 in range(len(dataList) - 1):
+            if lm.compare(num2, num1) >= 0:
+                lm.exchange(num2, num1)
+            else:
+                continue
+
+
+if __name__ == "__main__":
+    lm = ListManager()
+
+    sort(lm.dataList)
+
+    lm.checkResult()
+
+# 完成までの時間: 79分
